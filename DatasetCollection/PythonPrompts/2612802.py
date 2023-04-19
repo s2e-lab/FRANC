75871f8be21a46bef16b0626f8d12e2a987a9182
Joanna C. S. Santos <9554167+joannacss@users.noreply.github.com>
2023-04-19 16:47:43 -0400

    Update 2612802.py

--- DatasetCollection/PythonPrompts/2612802.py
+++ DatasetCollection/PythonPrompts/2612802.py
@@ -1,6 +1,9 @@
 def deep_copy_list(list_to_copy: list) -> list:
     '''
-    Returns a deep copy of the list passed in.
+    Returns a deep copy of the list passed in. For instance:
+    a = [1, 2, 3]
+    b = deep_copy_list(a)
+    c = id(a) != id(b) # c is true
 
     Example:
     deep_copy_list([1, 2, 3])
