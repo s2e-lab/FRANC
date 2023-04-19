346898ac4d8d3f7bb707bd2e0d66c56c13348dd2
Joanna C. S. Santos <9554167+joannacss@users.noreply.github.com>
2023-04-19 16:25:03 -0400

    Update 23294658.py

--- DatasetCollection/PythonPrompts/23294658.py
+++ DatasetCollection/PythonPrompts/23294658.py
@@ -1,7 +1,7 @@
 def is_valid_voter():
     '''
-    Take user input for age of a voter until a valid age is entered
-    Check if the voter is eligible to vote if the age is greater than 18
+    Take user input for age of a voter until a valid age is entered.
+    Check if the voter is eligible to vote if the age is greater than or equals to 18.
     
     Example:
     is_valid_voter() -> 'You are eligible to vote'
