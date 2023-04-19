c3237c587f8ba4ff90ca892e0aff049f487dbb0f
Joanna C. S. Santos <9554167+joannacss@users.noreply.github.com>
2023-04-19 16:50:13 -0400

    Update 1663807.py

--- DatasetCollection/PythonPrompts/1663807.py
+++ DatasetCollection/PythonPrompts/1663807.py
@@ -1,6 +1,6 @@
 def parallel_iteration(list1: list, list2: list) -> int:
     '''
-    Return the sum of the products of the elements in list1 and list2.
+    Iterate list1 and list2 in parallel and return the sum of the products of the elements in list1 and list2.
     
     Example:
     parallel_iteration([1, 2, 3], [4, 5, 6]) -> 32
