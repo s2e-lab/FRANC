f2937c1a98a6e926f98148423310576a89ec15af
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-04-24 02:02:52 -0400

    added one java prompt

---
+++ DatasetCollection/JavaPrompts/76049211.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+
+class ID_76049211{
+    /**
+     * Write a function to get the last index in a linked list of a given value.
+     * 
+     * Example:
+     * LinkedList<Integer> list = new LinkedList<Integer>();
+     * list.add(1);
+     * list.add(2);
+     * list.add(3);
+     * list.add(2);
+     * list.add(4);
+     * list.add(2);
+     * 
+     * int index = getLastIndex(list, 2);
+     * 
+     * output: 5
+     */
+    public static int getLastIndex(LinkedList<Integer> list, int value) {
