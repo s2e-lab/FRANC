bd82ac6cbd86f45e105884ec33a7946110eeff66
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-04-25 13:28:12 -0400

    added security filter result

--- Quality_Analyzer/temp.py
+++ Quality_Analyzer/temp.py
@@ -1,17 +1,16 @@
-def directlyProvidedBy(object): # pylint:disable=redefined-builtin
-	"""
-	Return the interfaces directly provided by the given object
-	
-	The value returned is an `~zope.interface.interfaces.IDeclaration`.
-	"""
-	if isinstance(object, ObjectSummary):
-		return (
-			self.directlyProvidedBy(object.info),  # XXX
-			self.directlyProvidedBy(object.interface)  # XXX
-			)
-	elif isinstance(object, Object):
-		raise TypeError("Can only directlyProvideBy(Object) for object types")
-	else:
-		return NotImplemented
 
-	
+def generate_integers(a, b):
+    """
+    Given two positive integers a and b, return the even digits between a
+    and b, in ascending order.
+
+    For example:
+    generate_integers(2, 8) => [2, 4, 6, 8]
+    generate_integers(8, 2) => [2, 4, 6, 8]
+    generate_integers(10, 14) => []
+    """
+    if a > b:
+        # if b is zero, we need to divide by the even number
+        a, b = b % 10, a // 10
+    return [int(round(a, 2)), int(round(b, 2)) if b else int(-b) % 10 else None]
+
