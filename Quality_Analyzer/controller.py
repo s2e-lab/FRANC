34b7e9e7c598072e9bf41454a55f97c594547957
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-04-25 08:52:17 -0400

    updated controller code

--- Quality_Analyzer/controller.py
+++ Quality_Analyzer/controller.py
@@ -4,13 +4,13 @@ from utils import get_prompts, write_suggestions
 
 from analyzer import java_analyzer, python_analyzer
 
-benchmark_root = "../SuggestionGenerator/Suggestions/"
+benchmark_root = "../Static_Filter/Static_Filtered_Suggestions/"
 dir_list = os.listdir(benchmark_root)
 
 for benchmark_file in dir_list:
     if ".DS_Store" not in benchmark_file:
         print("Processing file: ", benchmark_file)
-        benchmark_path = benchmark_root + benchmark_file
+        benchmark_path = os.path.join(benchmark_root, benchmark_file)
         prompts = get_prompts(benchmark_path)
         print("Number of prompts: ", len(prompts))
 
@@ -19,7 +19,7 @@ for benchmark_file in dir_list:
 
         dataset_type = "Python"
         suggestion_root = "./Static_Filtered_Suggestions/   "
-        suggestion_path = suggestion_root + benchmark_file
+        suggestion_path = os.path.join(suggestion_root, benchmark_file)
         suggestions = []
         if dataset_type == "Python":
             suggestions = python_analyzer()
